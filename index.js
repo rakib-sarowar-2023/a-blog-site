f91395e97f3254b60e07b256aef553e887c91e47
Rakib Sarowar <rakibsarowar2@gmail.com>
2023-05-21 18:33:58 +0600

    added the code

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function multiplicationTable(num) {
+
+  for (let i = 1; i <= 10; i++) {
+    const product = num * i;
+    console.log(`${num} x ${i} = ${product}`);
+  }
+}
+
+// Testing
+multiplicationTable(3);
+multiplicationTable(4);
